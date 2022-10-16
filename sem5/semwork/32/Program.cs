410b66ceb0cfa7b25463bdd7d7bb0c75c4286526
Movek <nikromaz@gmail.com>
2022-10-16 19:29:13 +0300

    Собрал задачи из занятий в общую папку

---
+++ sem5/semwork/32/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 32: Напишите программу замена элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue);
+    }
+    return res;
+}
+
+void Reverse(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = -array[i];
+    }
+}
+Console.Clear();
+int[] array = GetArray(5, -10, 10);
+Console.WriteLine(String.Join(", ", array));
+Reverse(array);
+Console.WriteLine(String.Join(", ", array));
