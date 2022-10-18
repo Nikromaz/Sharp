59df8b687937d8a4e7ccee8ae4f2a74c1c0be5f4
Movek <nikromaz@gmail.com>
2022-10-18 22:03:41 +0300

    1

--- sem5/homework/34/Program.cs
+++ sem5/homework/34/Program.cs
@@ -2,36 +2,24 @@
 //трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
-//Задача 31.
+//Брал за основу Задача 31.
 //Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
 //Найдите сумму отрицательных и положительных элементов массива.
 
 int[] GetArray(int size, int minValue, int maxValue) //Функция создания массива
 {
     int[] res = new int[size];
-
     for (int i = 0; i < size; i++)
     {
         res[i] = new Random().Next(minValue, maxValue + 1);
     }
     return res;
 }
-void PrintArray(int[] mass) //Функция вывода массива
+int PrintArray(int[] mass) //Функция вывода массива
 {
-    Console.Write("[");
-    for (int i = 0; i < mass.Length; i++)
-    {
-        Console.Write(mass[i]);
-        if (i != mass.Length - 1)
-        {
-            Console.Write(", ");
-        }
-    }
-    Console.Write("]");
+
 }
-
-
 Console.Clear();
 int[] array = GetArray(4, 111, 999);
 Console.WriteLine(String.Join(", ", array));
-Console.WriteLine($"{PrintArray} -> {CheckParity}");
+Console.WriteLine(PrintArray);
